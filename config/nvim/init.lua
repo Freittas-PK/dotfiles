0ed22f8f45ff9c513091bfb418a610e290013e8a
Freittas-PK <kallebfreittas@gmail.com>
2026-04-21 06:41:18 -0400

    Restructure waybar and remove all styles

---
+++ config/nvim/init.lua
@@ -0,0 +1,8 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.termguicolors = true
+vim.cmd("colorscheme desert")
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
